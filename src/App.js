0c93243df570cb15cb9479c01df0673159be05f9
Piotr Macuk <piotr@macuk.pl>
2020-04-29 21:57:38 +0200

    More pomodoro + first form

--- src/App.js
+++ src/App.js
@@ -1,9 +1,30 @@
 import React from 'react';
 import './App.css';
 
-function App() {
+const Form = () => {
   return (
-    <h1>Hello, World!</h1>
+    <form>
+      <div className="form-row">
+        <div className="form-group col-md-6">
+          <label htmlFor="task">Task</label>
+          <input type="text" className="form-control" id="task" />
+        </div>
+        <div className="form-group col-md-6">
+          <label htmlFor="time">Time</label>
+          <input type="text" className="form-control" id="time" />
+        </div>
+      </div>
+      <button type="submit" className="btn btn-primary">Add</button>
+    </form>
+  )
+}
+
+const App = () => {
+  return (
+    <>
+      <h1>🍅 Pomodoro Timer</h1>
+      <Form />
+    </>
   );
 }
 
