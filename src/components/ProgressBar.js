cb7561a0e0ff04f42642e488da4aaf2d3f3157c5
Piotr Macuk <piotr@macuk.pl>
2020-05-15 09:48:53 +0200

    Use better name

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -1,15 +1,15 @@
 import React from "react"
 
-const ProgressBar = ({prc}) => {
+const ProgressBar = ({percent}) => {
   return (
     <div className="progress">
       <div
         className="progress-bar bg-danger"
         role="progressbar"
-        style={{width: `${prc}%`}}
-        aria-valuenow={prc}
+        style={{width: `${percent}%`}}
+        aria-valuenow={percent}
         aria-valuemin="0"
-        aria-valuemax="100">{prc}%
+        aria-valuemax="100">{percent}%
       </div>
     </div>
   )
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -9,17 +9,17 @@ const Timer = ({id, task, time, handleRemove}) => {
   const [status, setStatus] = useState('stopped')
   const [pauses, setPauses] = useState(0)
   const [secondsLeft, setSecondsLeft] = useState(time * 60)
-  const [prc, setPrc] = useState(0)
+  const [percent, setPercent] = useState(0)
 
   useEffect(() => {
     const timer = setTimeout(() => {
       if (status === 'started' && secondsLeft > 0) {
         setSecondsLeft(secondsLeft - 1)
-        setPrc(100 - Math.floor((secondsLeft / (time * 60)) * 100))
+        setPercent(100 - Math.floor((secondsLeft / (time * 60)) * 100))
       }
     }, 1000)
     return () => clearInterval(timer)
-  }, [status, secondsLeft, prc, time])
+  }, [status, secondsLeft, percent, time])
 
   const handleStart = () => {
     setStatus('started')
@@ -29,7 +29,7 @@ const Timer = ({id, task, time, handleRemove}) => {
     setStatus('stopped')
     setPauses(0)
     setSecondsLeft(time * 60)
-    setPrc(0)
+    setPercent(0)
   }
 
   const handlePause = () => {
@@ -59,7 +59,7 @@ const Timer = ({id, task, time, handleRemove}) => {
           <Clock secondsLeft={secondsLeft}/>
         </div>
         <div className="col-md-8">
-          <ProgressBar prc={prc}/>
+          <ProgressBar percent={percent}/>
         </div>
         <div className="col-md-2">
           <Pauses pauses={pauses}/>
